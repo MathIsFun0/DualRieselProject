0a3fc41e7c652fe4d3691d3be713eead9ee9b3be
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2022-08-09 19:15:54 -0400

    Clear website branch

--- initialization.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INITIALIZATION_HPP
-#define INITIALIZATION_HPP
-
-//Contains all initialization and initialization-related functions
-//Does NOT contain initial testing
-void initialization(Options options);
-
-#endif
