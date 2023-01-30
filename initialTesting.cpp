9f83962ec488cbdb93b7b93fc18c2e6cf1dc62ec
lmesh91 <89877664+MathIsFun0@users.noreply.github.com>
2023-01-29 21:34:02 -0500

    Untested boost::async and commenting

--- initialTesting.cpp
+++ initialTesting.cpp
@@ -7,9 +7,11 @@
 #include <cmath>
 #include <cstdlib>
 #include <fstream>
-#include <future>
 #include <string>
-#include <thread>
+
+#define BOOST_THREAD_VERSION 4
+#include <boost/thread.hpp>
+#include <boost/thread/future.hpp>
 
 #ifdef _WIN32
 #define EXECPREFIX ""
@@ -20,6 +22,8 @@
 
 
 void initialTesting(Options options, Conjecture conjecture) {
+    //Setup phase
+    //Testing limit to certain # of digits, or maxN if smaller
     int initTestingLimit = std::min((double)conjecture.maxN,std::floor((double)options.initialTestingCutoff/std::log10(conjecture.base)));
     std::string sign = "-";
     if (conjecture.conjectureType == "S") {
@@ -27,18 +31,21 @@ void initialTesting(Options options, Conjecture conjecture) {
     }
     int maxThreads = options.pfgwInstances;
     bool starting = true;
+
     while (conjecture.minN <= initTestingLimit) {
         if (starting) {
             starting = false;
             println("Testing small values - n < " + std::to_string(initTestingLimit));
         }
         std::vector<std::string> candidateString;
-        if (conjecture.candidates.size() > 100000) {
+        if (conjecture.candidates.size() > 100000) { //Print warning...
             println("Creating candidate list. This may take a while...");
         }
         candidateString.reserve(conjecture.candidates.size());
         for (long candidate : conjecture.candidates) {
+            //# Filter only + values for Rp
             if (conjecture.conjectureType != "Rp" || std::log(candidate) / std::log(conjecture.base) < conjecture.minN) {
+                //# Add string representation to vector
                 candidateString.push_back(std::to_string(conjecture.base)+"^"+std::to_string(conjecture.minN)+sign+std::to_string(candidate));
             }
         }
@@ -48,18 +55,19 @@ void initialTesting(Options options, Conjecture conjecture) {
         std::string basePath = "temp/"+conjecture.fileName+"-"+std::to_string(conjecture.minN)+"_";
         for (int i = 0; i < numInstances; i++) {
             std::string fileName = basePath + std::to_string(i)+".txt";
+            //Save to file
             std::ofstream thisFile(fileName);
             thisFile << vec2string(candidateThreadLists[i], true);
             thisFile.close();
         }
         println("Testing n = "+std::to_string(conjecture.minN));
-        std::vector<std::future<int>> futureOutcomes;
+        std::vector<boost::future<int>> futureOutcomes;
         futureOutcomes.reserve(numInstances);
         for (int i = 0; i < numInstances; i++) {
             //makes and goes to temp/pfgwX, then runs it.
             //wrapped in async call so that it's, well.. async.
             //todo: support windows with boost (https://stackoverflow.com/questions/45565412/invalid-use-of-incomplete-type-using-stdfuture)
-            futureOutcomes.push_back(std::async(std::launch::async, systemWrapper, 
+            futureOutcomes.push_back(boost::async(boost::launch::async, systemWrapper, 
             "cd temp; mkdir -p pfgw"+std::to_string(i)+";cd pfgw"+std::to_string(i)+";"+
             EXECPREFIX+"../../"+options.pfgwPath+" -T"+std::to_string(options.pfgwThreadsPerInstance)+ " ../../"+basePath+std::to_string(i)+".txt"));  
         }
